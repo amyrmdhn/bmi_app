6a7d45d8c3512610f9f27d9b820f741bec150db2
amyrmdhn <amyramadhan99@gmail.com>
2024-04-12 20:53:14 +0700

    feat(view model): add encapsulated toggle switch state and corresponding manipulation function

--- lib/view_model/bmi_view_model.dart
+++ lib/view_model/bmi_view_model.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 
-class BmiViewModel extends ChangeNotifier {}
+class BmiViewModel extends ChangeNotifier {
+  var _isFemale = 1;
+
+  int get isFemale => _isFemale;
+
+  void setIsFemale(int isFemale) {
+    _isFemale = isFemale;
+    notifyListeners();
+  }
+}
