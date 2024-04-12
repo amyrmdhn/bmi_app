0103284e3dab95e1a0a3fb6ff425727bb2c10a5a
amyrmdhn <amyramadhan99@gmail.com>
2024-04-12 21:27:23 +0700

    feat(calculate screen): use BmiToggleSwitch with Selector to manage independent state in the view model

--- lib/view/screens/bmi_calculate.dart
+++ lib/view/screens/bmi_calculate.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:bmi_app/helper.dart';
+import '../widgets/bmi_toggle_switch.dart';
+import '../../view_model/bmi_view_model.dart';
 
 class BmiCalculateScreen extends StatefulWidget {
   const BmiCalculateScreen({super.key});
@@ -14,8 +17,24 @@ class _BmiCalculateScreenState extends State<BmiCalculateScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
-      body: const Center(
-        child: Text('Initialization'),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: Column(
+          children: [
+            //* Switch for Male or Female BMI Count Configuration
+            Selector<BmiViewModel, int>(
+              builder: (context, isFemale, child) {
+                return BMIToggleSwitch(
+                  isFemale: isFemale,
+                  onToggle: (index) =>
+                      context.read<BmiViewModel>().setIsFemale(index!),
+                );
+              },
+              selector: (context, viewModel) => viewModel.isFemale,
+            ),
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
