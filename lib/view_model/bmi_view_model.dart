2fde08b362356f639a79cc5da5459c384bd47dde
amyrmdhn <amyramadhan99@gmail.com>
2024-04-12 22:12:32 +0700

    feat(view model): add encapsulated weight and age picker state and corresponding manipulation function

--- lib/view_model/bmi_view_model.dart
+++ lib/view_model/bmi_view_model.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class BmiViewModel extends ChangeNotifier {
   var _isFemale = 1;
   var _height = 170;
+  var _weight = 72;
+  var _age = 25;
 
   int get isFemale => _isFemale;
   int get height => _height;
+  int get weight => _weight;
+  int get age => _age;
 
   void setIsFemale(int isFemale) {
     _isFemale = isFemale;
@@ -14,6 +18,16 @@ class BmiViewModel extends ChangeNotifier {
 
   void setHeight(int height) {
     _height = height;
+    notifyListeners();
+  }
+
+  void setWeight(int weight) {
+    _weight = weight;
+    notifyListeners();
+  }
+
+  void setAge(int age) {
+    _age = age;
     notifyListeners();
   }
 
