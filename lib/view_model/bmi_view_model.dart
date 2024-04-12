6b6ec3a94954bbfe94f4934f918061a9394ddc2c
amyrmdhn <amyramadhan99@gmail.com>
2024-04-12 21:48:14 +0700

    feat(view model): add encapsulated height picker state and corresponding manipulation function

--- lib/view_model/bmi_view_model.dart
+++ lib/view_model/bmi_view_model.dart
@@ -2,11 +2,28 @@ import 'package:flutter/material.dart';
 
 class BmiViewModel extends ChangeNotifier {
   var _isFemale = 1;
+  var _height = 170;
 
   int get isFemale => _isFemale;
+  int get height => _height;
 
   void setIsFemale(int isFemale) {
     _isFemale = isFemale;
+    notifyListeners();
+  }
+
+  void setHeight(int height) {
+    _height = height;
+    notifyListeners();
+  }
+
+  void increment() {
+    _height++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _height--;
     notifyListeners();
   }
 }
