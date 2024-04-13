71ee403ac76054f978c66ccde4747182ff8c4e1a
amyrmdhn <amyramadhan99@gmail.com>
2024-04-13 21:05:42 +0700

    feat(result screen): add headline text to show user bmi category

--- lib/view/screens/bmi_result.dart
+++ lib/view/screens/bmi_result.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '/helper.dart';
+import '../../view_model/bmi_view_model.dart';
 
 class BmiResultScreen extends StatelessWidget {
   const BmiResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Initialization');
+    final size = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: appBar,
+      body: Center(
+        child: Consumer<BmiViewModel>(
+          builder: (context, viewModel, child) {
+            return Column(
+              children: [
+                const Spacer(),
+                //* Headline Text : BMI Category
+                Text(
+                  viewModel.person!.bmiCategories,
+                  style: const TextStyle(
+                    color: Color(0xFF535456),
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                  ),
+                ),
+                const Spacer(),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 }
